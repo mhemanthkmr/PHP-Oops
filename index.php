cf1aad6f44552774e127a7033c433c38ab0b9cc1
mhemanthkmr <m.hemanthkmr143@gmail.com>
2022-02-02 18:37:50 +0530

    Added Nav

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
 <?php 
+    include('includes/header.php');
+    include('includes/navbar.php');
+?>
+<h1 class="">DashBoard</h1>
+<?php 
+    include('includes/footer.php');
+?>
---
+++ login.php
@@ -0,0 +1,21 @@
+<?php 
+    include('includes/header.php');
+    include('includes/navbar.php');
+?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-5 text-center">
+            <div class="form-floating mb-3">
+                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                <label for="floatingInput">Email address</label>
+            </div>
+            <div class="form-floating">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                <label for="floatingPassword">Password</label>
+            </div>
+        </div>
+    </div>
+</div>
+<?php 
+    include('includes/footer.php');
+?>
