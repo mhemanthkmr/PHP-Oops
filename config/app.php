474d49552d5b2390ca42ca8c60658ee2bede7af9
mhemanthkmr <m.hemanthkmr143@gmail.com>
2022-03-15 22:52:31 +0530

    Defined Database Configuration

---
+++ config/app.php
@@ -0,0 +1,9 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'mhemanthkmr');
+define('DB_PASSWORD', 'hemanth123');
+define('DB_DATABASE', 'Oops');
+
+include_once('DatabaseConnection.php');
+$db = new DatabaseConnection;
+?>
