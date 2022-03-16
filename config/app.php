f05c2c498693e623a6fee56a95fd6b3b971fb314
mhemanthkmr <m.hemanthkmr143@gmail.com>
2022-03-16 23:01:47 +0530

    Added Redirected Code

--- config/app.php
+++ config/app.php
@@ -4,7 +4,7 @@ define('DB_USER', 'mhemanthkmr');
 define('DB_PASSWORD', 'hemanth123');
 define('DB_DATABASE', 'Oops');
 define('SITE_URL', 'http://localhost/PHP-Oops/');
-
+include_once('controllers/SessionController.php');
 include_once('DatabaseConnection.php');
 $db = new DatabaseConnection;
 
@@ -16,8 +16,9 @@ function validateInput($dbcon,$input)
 {
     return mysqli_real_escape_string($dbcon, $input);
 }
-function redirect($msg,$page)
+function redirect($name,$msg,$page)
 {
-
+    Session::msg($name, $msg);
+    Session::head($page);
 }
 ?>
