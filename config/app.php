f7cf635b5ecb81ad62728e99e4071812dcb45215
mhemanthkmr <m.hemanthkmr143@gmail.com>
2022-03-16 21:56:26 +0530

    Created Function Base Url ValidateInput

--- config/app.php
+++ config/app.php
@@ -3,7 +3,21 @@ define('DB_HOST', 'localhost');
 define('DB_USER', 'mhemanthkmr');
 define('DB_PASSWORD', 'hemanth123');
 define('DB_DATABASE', 'Oops');
+define('SITE_URL', 'http://localhost/PHP-Oops/');
 
 include_once('DatabaseConnection.php');
 $db = new DatabaseConnection;
+
+function baseurl($slug)
+{
+    echo SITE_URL.$slug;
+}
+function validateInput($dbcon,$input)
+{
+    return mysqli_real_escape_string($dbcon, $input);
+}
+function redirect($msg,$page)
+{
+
+}
 ?>
