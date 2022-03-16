2161d8a411fbd37e33fb6013d21990d92120dfdd
mhemanthkmr <m.hemanthkmr143@gmail.com>
2022-03-16 23:00:14 +0530

    Created Session Controler

--- controllers/SessionController.php
+++ controllers/SessionController.php
@@ -1,5 +1,20 @@
 <?php
-class SessionController
+class Session
 {
-    
+    public static function start()
+    {
+        session_start();
+    }
+    public static function msg($name,$msg)
+    {
+        $_SESSION[$name] = $msg;
+    }
+    public static function head($page)
+    {
+        header("Location: ".$page);
+    }
+    public static function unset()
+    {
+        session_unset();
+    }
 }
