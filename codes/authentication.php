beca5a6e430e3d2cdc0e057fbabf500f250920dc
mhemanthkmr <m.hemanthkmr143@gmail.com>
2022-03-22 22:35:32 +0530

    Cleared Bug in Validate INput

--- codes/authentication.php
+++ codes/authentication.php
@@ -1,24 +1,26 @@
 <?php
-include('config/app.php');
-include('controller/RegisterController.php');
+include('../config/app.php');
+include('../controllers/RegisterController.php');
 if(isset($_POST['register_btn']))
 {
-    $name = validateInput($db->con,$_POST['name']);
-    $username = validateInput($db->con,$_POST['username']);
-    $email = validateInput($db->con,$_POST['email']);
-    $password = validateInput($db->con,$_POST['password']);
-    $cpassword = validateInput($db->con,$_POST['cpassword']);
-
+    $name = validateInput($db->conn,$_POST['name']);
+    $username = validateInput($db->conn,$_POST['username']);
+    $email = validateInput($db->conn,$_POST['email']);
+    $password = validateInput($db->conn,$_POST['password']);
+    $cpassword = validateInput($db->conn,$_POST['cpassword']);
+    // print_r($_POST);
+    echo $password.$cpassword;
     $register = new RegisterController;
 
     $result_password = $register->confirmPassword($password, $cpassword);
     if($result_password)
     {
-
+        echo "Password Match";
     }
     else
     {
-        redirect('status','Password and Confirm Password does not Match','index.php');
+        // redirect('status','Password and Confirm Password does not Match','index.php');
+        echo "Password Not Match";
     }
 }
 ?>
